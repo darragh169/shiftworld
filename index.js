c8f057a35e4d62d902d5bca9630dc89db1d6b135
jamesfinn180 <jamesfinn180@hotmail.com>
2016-11-07 23:44:16 -0800

    Switch gravity by pressing C. Sprite inverts as well.

--- index.js
+++ index.js
@@ -18,9 +18,13 @@ var layer;
 var player;
 var facing = 'left';
 var jumpTimer = 0;
+var gravityTimer = 0;
 var cursors;
 var jumpButton;
+var gravityButton;
 var bg;
+var playerSpeed = 220;
+var gravityDown = true;
 
 function create() {
 
@@ -30,6 +34,7 @@ function create() {
 
     bg = game.add.tileSprite(0, 0, 800, 600, 'background');
     bg.fixedToCamera = true;
+
 
     map = game.add.tilemap('level1');
 
@@ -44,14 +49,15 @@ function create() {
 
     layer.resizeWorld();
 
-    game.physics.arcade.gravity.y = 250;
+    game.physics.arcade.gravity.y = 350;
 
     player = game.add.sprite(32, 32, 'dude');
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
-    player.body.bounce.y = 0.2;
+    player.body.bounce.y = 0.0; // I set this to 0 because it interfers with the jump. Originally 0.2
     player.body.collideWorldBounds = true;
     player.body.setSize(20, 32, 5, 16);
+    player.anchor.setTo(0.5, 0.5);  // This ensure that the player's centre point is in the middle. Needed for flipping sprite
 
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('turn', [4], 20, true);
@@ -61,18 +67,18 @@ function create() {
 
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    gravityButton = game.input.keyboard.addKey(Phaser.Keyboard.C);      // Press C to flip gravity
 
 }
 
 function update() {
-
     game.physics.arcade.collide(player, layer);
 
     player.body.velocity.x = 0;
 
     if (cursors.left.isDown)
     {
-        player.body.velocity.x = -150;
+        player.body.velocity.x = -playerSpeed;
 
         if (facing != 'left')
         {
@@ -82,7 +88,7 @@ function update() {
     }
     else if (cursors.right.isDown)
     {
-        player.body.velocity.x = 150;
+        player.body.velocity.x = playerSpeed;
 
         if (facing != 'right')
         {
@@ -111,16 +117,27 @@ function update() {
     
     if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
     {
-        player.body.velocity.y = -250;
+        player.body.velocity.y =  -250;
+
         jumpTimer = game.time.now + 750;
+    }
+
+    // Reversing GRAVITY when C button is pressed
+    if(gravityButton.isDown && game.time.now > gravityTimer) {  
+        gravityDown = !gravityDown              // Change gravity boolean
+        game.physics.arcade.gravity.y *= -1;    // Invert gravity
+
+        //player.anchor.setTo(0.5, 0.5);        // Set anchor point to middle of sprite - Redundant due to setting this at create()
+        player.scale.y *= -1;                   // Flip Sprite vertically
+        gravityTimer = game.time.now + 500;     // Ensures that function is called once 
     }
 
 }
 
-function render () {
-
-    // game.debug.text(game.time.physicsElapsed, 32, 32);
-    // game.debug.body(player);
-    // game.debug.bodyInfo(player, 16, 24);
-
+function render() {
+    /*
+    game.debug.text(game.time.physicsElapsed, 32, 32);
+    game.debug.body(player);
+    game.debug.bodyInfo(player, 16, 24);
+    */
 }
